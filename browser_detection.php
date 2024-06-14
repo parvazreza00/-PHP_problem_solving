4b415fdee6d83183f00301919641e3be6dfb8e29
parvaz <parvazreza00@gmail.com>
2024-06-14 11:34:01 +0600

    browser detection

---
+++ browser_detection.php
@@ -0,0 +1,16 @@
+<!-- php code for user browser detection -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Browser Detection</title>
+</head>
+<body>
+<?php
+
+echo "Your user agnet is: ". $_SERVER['HTTP_USER_AGENT'];
+
+?>
+</body>
+</html>
