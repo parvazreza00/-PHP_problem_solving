5cb2754bd445ef5e34bace4b8dbf34c8bf311b37
parvaz <parvazreza00@gmail.com>
2024-06-25 10:55:13 +0600

    file is updated

--- hex_to_binary.php
+++ hex_to_binary.php
@@ -2,11 +2,19 @@
 // conversion hexadecimal to binary in php
 echo "Enter a HexaDecimal Number:\n";
 $hex = trim(fgets(STDIN));
-$dec = hexdec($hex);//firstly hex to dec
-$bin = decbin($dec);// and then dec to binary
-$paddedbinary = str_pad($bin, strlen($hex) * 4, '0', STR_PAD_LEFT);
-echo "$hex = $paddedbinary";
-//finally hex to binary conversion
+if(preg_match('/^[0-9a-fA-F]+$/',$hex)){
+    //firstly hex to dec
+    $dec = hexdec($hex);
+    // $bin = decbin($dec);// and then dec to binary
+    // and then dec to binary
+    $bin = base_convert($dec,10,2);
+    $paddedbinary = str_pad($bin, strlen($hex) * 4, '0', STR_PAD_LEFT);
+    echo "$hex = $paddedbinary";
+    //finally hex to binary conversion
+}else{
+    echo "Invalid Number\n";
+}
+
 
 
 ?>
