7b144c7abd4eeb9119bcd1d6244e2518cac2e646
parvaz <parvazreza00@gmail.com>
2024-06-28 10:03:00 +0600

    even numbers count, numbers, and position

---
+++ count_even_nbr.php
@@ -0,0 +1,24 @@
+<?php
+// a PHP program to count even number and their position from a given array;
+
+$arr = [3,9,6,1,4,8,11,18];
+$evens = 0;
+
+
+for($i = 0; $i<sizeof($arr); $i++){
+    if($arr[$i] % 2 == 0){
+        $positions[] = $i;
+        $event_number[] = $arr[$i];
+        $evens++;
+    }
+        
+}
+echo "Number of even number: ".$evens."\n";
+
+foreach($positions as $posi){
+    echo $posi." ";
+}
+echo "\n";
+foreach($event_number as $n){
+    echo $n." ";
+}
