cfa2817de26cf09431c9902b820bbceeb425b1b2
parvaz <parvazreza00@gmail.com>
2024-07-01 10:02:38 +0600

    traingle_star1.php updated

--- triangle_star1.php
+++ triangle_star1.php
@@ -1,31 +1,64 @@
 <?php
 
 echo "Enter a Row number:\n";
-$number = trim( fgets( STDIN ) );
-for ( $i = 1; $i <= $number; $i++ ) {
-    for ( $j = 1; $j <= $i; $j++ ) {
+$number = trim(fgets(STDIN));
+for ($i = 1; $i <= $number; $i++) {
+    for ($j = 1; $j <= $i; $j++) {
         echo " * ";
     }
     echo "\n";
 }
 
 echo "\n";
-for ( $i = 1; $i <= $number; $i++ ) {
-    for ( $j = $number - $i; $j >= 0; $j-- ) {
+for ($i = 1; $i <= $number; $i++) {
+    for ($j = $number - $i; $j >= 0; $j--) {
         echo " * ";
     }
     echo "\n";
 }
 
 
-for($i = 0; $i<=$number; $i++){
-    for($k = 5; $k>=$i; $k--){
+for ($i = 0; $i <= $number; $i++) {
+    for ($k = $number; $k >= $i; $k--) {
         echo " ";
     }
 
-    for($j = 1; $j <= $i; $j++){
+    for ($j = 1; $j <= $i; $j++) {
         echo "* ";
     }
 
-    echo "\n";  
+    echo "\n";
 }
+echo "\n";
+
+for ($i = 1; $i <= $number; $i++) {
+    for ($k = 1; $k <= $i; $k++) {
+        echo " ";
+    }
+    for ($j = $number; $j >= $i; $j--) {
+        echo "* ";
+    }
+    echo "\n";
+}
+
+echo "\n";
+
+for ($i = 1; $i <= $number; $i++) {
+    for ($k = $number; $k >= $i; $k--) {
+        echo " ";
+    }
+    for($j=1; $j <= $i; $j++){
+        echo "* ";
+    }
+    echo "\n";
+}
+for($i=0; $i<=$number; $i++){
+    for($k=1; $k<=$i; $k++){
+        echo " ";
+    }
+    for($j=$number; $j>=$i; $j--){
+        echo "* ";
+    }
+    echo "\n";
+}
+
