4bc8f1456fdba781ad2e1203d495e641db9e308a
parvaz <parvazreza00@gmail.com>
2024-07-02 22:00:54 +0600

    triangle_star1.php updated

--- triangle_star1.php
+++ triangle_star1.php
@@ -1,29 +1,28 @@
 <?php
 
 echo "Enter a Row number:\n";
-$number = trim(fgets(STDIN));
-for ($i = 1; $i <= $number; $i++) {
-    for ($j = 1; $j <= $i; $j++) {
+$number = trim( fgets( STDIN ) );
+for ( $i = 1; $i <= $number; $i++ ) {
+    for ( $j = 1; $j <= $i; $j++ ) {
         echo " * ";
     }
     echo "\n";
 }
 
 echo "\n";
-for ($i = 1; $i <= $number; $i++) {
-    for ($j = $number - $i; $j >= 0; $j--) {
+for ( $i = 1; $i <= $number; $i++ ) {
+    for ( $j = $number - $i; $j >= 0; $j-- ) {
         echo " * ";
     }
     echo "\n";
 }
 
-
-for ($i = 0; $i <= $number; $i++) {
-    for ($k = $number; $k >= $i; $k--) {
+for ( $i = 0; $i <= $number; $i++ ) {
+    for ( $k = $number; $k >= $i; $k-- ) {
         echo " ";
     }
 
-    for ($j = 1; $j <= $i; $j++) {
+    for ( $j = 1; $j <= $i; $j++ ) {
         echo "* ";
     }
 
@@ -31,11 +30,11 @@ for ($i = 0; $i <= $number; $i++) {
 }
 echo "\n";
 
-for ($i = 1; $i <= $number; $i++) {
-    for ($k = 1; $k <= $i; $k++) {
+for ( $i = 1; $i <= $number; $i++ ) {
+    for ( $k = 1; $k <= $i; $k++ ) {
         echo " ";
     }
-    for ($j = $number; $j >= $i; $j--) {
+    for ( $j = $number; $j >= $i; $j-- ) {
         echo "* ";
     }
     echo "\n";
@@ -43,20 +42,20 @@ for ($i = 1; $i <= $number; $i++) {
 
 echo "\n";
 
-for ($i = 1; $i <= $number; $i++) {
-    for ($k = $number; $k >= $i; $k--) {
+for ( $i = 1; $i <= $number; $i++ ) {
+    for ( $k = $number; $k >= $i; $k-- ) {
         echo " ";
     }
-    for($j=1; $j <= $i; $j++){
+    for ( $j = 1; $j <= $i; $j++ ) {
         echo "* ";
     }
     echo "\n";
 }
-for($i=0; $i<=$number; $i++){
-    for($k=1; $k<=$i; $k++){
+for ( $i = 0; $i <= $number; $i++ ) {
+    for ( $k = 1; $k <= $i; $k++ ) {
         echo " ";
     }
-    for($j=$number; $j>=$i; $j--){
+    for ( $j = $number; $j >= $i; $j-- ) {
         echo "* ";
     }
     echo "\n";
@@ -64,48 +63,41 @@ for($i=0; $i<=$number; $i++){
 
 echo "\n";
 
-for($i = 1;$i<=$number; $i++){
-    for($j=1;$j<=$i; $j++){
+for ( $i = 1; $i <= $number; $i++ ) {
+    for ( $j = 1; $j <= $i; $j++ ) {
         echo " * ";
     }
     echo "\n";
 }
-for($i = 1; $i <= $number; $i++){
-    for($j=$number; $j>=$i; $j--){
-        echo " * ";
-    }
-    echo "\n";
-}
-
-echo "\n";
-for($i = 0; $i<=$number; $i++){
-    for($j=0; $j<=$number;$j++){
+for ( $i = 1; $i <= $number; $i++ ) {
+    for ( $j = $number; $j >= $i; $j-- ) {
         echo " * ";
     }
     echo "\n";
 }
 
 echo "\n";
-for($i=5; $i>=1; $i--)  
-{  
-if($i%2 != 0)  
-{  
-for($j=5; $j>=$i; $j--)  
-{  
-echo "* ";  
-}  
-echo "\n";  
-}  
-}  
-for($i=2; $i<=5; $i++)  
-{  
- if($i%2 != 0)  
-{  
- for($j=5; $j>=$i; $j--)  
-{  
-echo "* ";  
-}  
-echo "\n";  
-}  
-}  
+for ( $i = 0; $i <= $number; $i++ ) {
+    for ( $j = 0; $j <= $number; $j++ ) {
+        echo " * ";
+    }
+    echo "\n";
+}
 
+echo "\n";
+for($i=$number; $i>=1; $i--){
+    if($i%2 != 0){
+        for($j=$number; $j>=$i; $j--){
+            echo " * ";
+        }
+        echo "\n";
+    }
+}
+for($i=2; $i<=$number; $i++){
+    if($i%2 != 0){
+        for($j = $number; $j>=$i; $j--){
+            echo " * ";
+        }
+        echo "\n";
+    }
+}
