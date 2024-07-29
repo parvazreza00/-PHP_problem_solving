7d040c363946530393a345284d8668d7240a1991
parvaz <parvazreza00@gmail.com>
2024-07-29 23:33:33 +0600

    string fragments and count

--- ostad_platform/array_walk.php
+++ ostad_platform/array_walk.php
@@ -4,4 +4,4 @@ function myfunction($value, $key, $p){
     echo "$key $p $value"."\n";
 }
 $a = array("a" => "red", "b"=>"green","c"=>"blue");
-array_walk($a, "myfunction", "has the value")
+array_walk($a, "myfunction", "has the value");
