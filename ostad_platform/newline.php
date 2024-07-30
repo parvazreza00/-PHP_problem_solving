dba0c320407987d0b9b87483a9192645218a20f0
parvaz <parvazreza00@gmail.com>
2024-07-30 18:17:41 +0600

    updated

--- ostad_platform/newline.php
+++ ostad_platform/newline.php
@@ -2,3 +2,4 @@
 $string = "Lorem ipsum dolor sit \namet consectetur adipisicing elit. Quidem doloribus quod id veniam dolore fugit aspernatur temporibus \nmolestiae laborum odit molestias blanditiis minima et consequuntur ullam incidunt, esse expedita dolores tempora. Ex possimus,\n cum illum qui recusandae obcaecati, dignissimos\n accusantium commodi corrupti dolore nostrum ad numquam non iure quas dicta!";
 
 echo nl2br($string);
+
