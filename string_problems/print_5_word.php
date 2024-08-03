539462785cb911cbdb0fefd7e6c785fe23680eb4
parvaz <parvazreza00@gmail.com>
2024-08-03 18:00:35 +0600

    updated

--- string_problems/print_5_word.php
+++ string_problems/print_5_word.php
@@ -2,8 +2,16 @@
 echo "<pre>";
 $my_string = "The quick brown fox jumps over the lazy dog";
 $arr_explode = explode(" ",$my_string);
-print_r($arr_explode);
+// print_r($arr_explode);
 $length = count($arr_explode);
+for($i=0;$i<=$length;$i++){
+    if($i < 5){
+        echo $arr_explode[$i].nl2br("\n");
+        
+    }else{
+        break;
+    }
+}
 // echo $length;
 
 
