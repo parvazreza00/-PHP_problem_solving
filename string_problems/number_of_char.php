b465290f2f718b279a7a555c72a830238c5afcf9
parvaz <parvazreza00@gmail.com>
2024-09-11 10:49:15 +0600

    updated

--- string_problems/number_of_char.php
+++ string_problems/number_of_char.php
@@ -5,6 +5,27 @@ echo $len."\n";
 $num_of_char = str_replace(" ", "", $str);
 $len2 = strlen($num_of_char);
 echo $len2;
+echo "\n";
+$count = 0;
+
+foreach(str_split($str) as $key => $char){
+if($char != ' ')
+
+    $count++;
+
+}
+echo $count;
+
+echo "\n";
+$length = 0;
+for($i = 0; isset($str[$i]); $i++){
+    if($str[$i] != ' '){
+        $length++;   
+    }   
+}
+echo $length;
+
+
 
 
 
