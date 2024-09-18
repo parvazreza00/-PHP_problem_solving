dfa4481c714518ea03a0c54b3b86dd9d84b75e56
parvaz <parvazreza00@gmail.com>
2024-09-18 10:03:08 +0600

    updated

--- string_problems/re_spe_char.php
+++ string_problems/re_spe_char.php
@@ -1,7 +1,7 @@
 <?php
 $input = trim(fgets(STDIN));
 if($input != ''){
-    $clean_str = preg_replace('/[^a-zA-Z0-9]/','',$input);
+    $clean_str = preg_replace('/[^a-zA-Z0-9]/s',' ',$input);
     echo $clean_str;
 }else{
     echo "You Don't insert any Input\n";
